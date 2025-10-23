725cfa0b0f5eebaddd2ad4c3a9473b48627f4731
Ahcene43 <benaakrabahcene@gmail.com>
2025-10-23 13:26:40 +0100

    Update store configuration - ٢٣‏/١٠‏/٢٠٢٥ ١:٢٦:٣٧ م

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ const STORE_CONFIG = {
       "image": "https://raw.githubusercontent.com/Ahcene43/WER/main/images/1761144949105-modal1.jpg",
       "description": "فستان الملكات الساحرات",
       "availableSizes": [
-        "S"
+        "S",
+        "M"
       ],
       "availableColors": [
-        "أبيض",
-        "أسود"
+        "كما في الصورة"
       ]
     }
   },
