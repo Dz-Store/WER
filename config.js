9e1e45f72e43a44bdfc67ab410d8e44b57e8e021
Dz-Store <benaakrabahcene@gmail.com>
2025-10-27 20:20:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const STORE_CONFIG = {
     "16": {
       "name": "مودال 1",
       "price": 3700,
-      "image": "https://raw.githubusercontent.com/dz-store/WER/main/images/1761141877869-modal12.jpg",
+      "image": "https://raw.githubusercontent.com/Dz-store/WER/main/images/1761141877869-modal12.jpg",
       "description": "فستان بمواصفات خيالية",
       "availableSizes": [
         "M",
@@ -33,7 +33,7 @@ const STORE_CONFIG = {
     "18": {
       "name": "مودال 3",
       "price": 5000,
-      "image": "https://raw.githubusercontent.com/dz-store/WER/main/images/product-18-1761331899479-modle6.jpg",
+      "image": "https://raw.githubusercontent.com/Dz-store/WER/main/images/product-18-1761331899479-modle6.jpg",
       "description": "فستان عالي الجودة",
       "availableSizes": [
         "S"
