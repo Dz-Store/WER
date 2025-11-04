d9c7c34146f45481adeb0db887f16b962a876d90
Dz-Store <benaakrabahcene@gmail.com>
2025-11-04 19:38:02 +0100

    تحديث إعدادات المتجر - ٤‏/١١‏/٢٠٢٥ ٧:٣٨:٠١ م

--- config.js
+++ config.js
@@ -262,10 +262,6 @@ const STORE_CONFIG = {
     "58 - المنيعة": {
       "home": 1000,
       "desk": 400
-    },
-    "59 - فرجيوة": {
-      "home": 900,
-      "desk": 500
     }
   },
   "DISCOUNTS": {
@@ -275,7 +271,7 @@ const STORE_CONFIG = {
       20,
       21
     ],
-    "discountScope": "all"
+    "discountScope": "selected"
   },
   "STORE_INFO": {
     "name": "BENAKRAB-Shopp",
