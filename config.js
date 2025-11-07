4fb579957eee30837dca8d497392bc963a1da947
Dz-Store <benaakrabahcene@gmail.com>
2025-11-07 21:52:08 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,375 @@
+const STORE_CONFIG = {
+  "PRODUCTS": {
+    "1": {
+      "name": "مودال 1",
+      "price": 6000,
+      "image": "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-1-1762411966732-6169e68ad981a5c61a35de6eb8ea92ca.jpg",
+      "description": "فستان تقليدي فاخر",
+      "availableSizes": [
+        "XL",
+        "S",
+        "S1",
+        "M2"
+      ],
+      "availableColors": [
+        "أحمر",
+        "كما في الصورة"
+      ],
+      "ageRanges": [
+        {
+          "ageFrom": 5,
+          "ageTo": 7,
+          "size": "S"
+        },
+        {
+          "ageFrom": 8,
+          "ageTo": 10,
+          "size": "M2"
+        },
+        {
+          "ageFrom": 11,
+          "ageTo": 12,
+          "size": "XL"
+        }
+      ]
+    },
+    "2": {
+      "name": "مودال 2",
+      "price": 6000,
+      "image": "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-2-1762299261628-modle6.jpg",
+      "description": "فستان الأفراح والمناسبات",
+      "availableSizes": [
+        "S"
+      ],
+      "availableColors": [
+        "وردي",
+        "أحمر",
+        "أبيض",
+        "كما في الصورة"
+      ],
+      "ageRanges": [
+        {
+          "ageFrom": 6,
+          "ageTo": 8,
+          "size": "S"
+        },
+        {
+          "ageFrom": 9,
+          "ageTo": 11,
+          "size": "S"
+        }
+      ]
+    },
+    "3": {
+      "name": "مودال 3",
+      "price": 5500,
+      "image": "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-3-1762376640949-littlebedouin-girls-luxury-occasion-dress-dress-231324.webp",
+      "description": "فستان الأفراح و المناسيات",
+      "availableSizes": [
+        "XXL"
+      ],
+      "availableColors": [
+        "كما في الصورة",
+        "أبيض"
+      ],
+      "ageRanges": [
+        {
+          "ageFrom": 12,
+          "ageTo": 14,
+          "size": "XXL"
+        },
+        {
+          "ageFrom": 15,
+          "ageTo": 16,
+          "size": "XXL"
+        }
+      ]
+    }
+  },
+  "DELIVERY_PRICES": {
+    "01 - أدرار": {
+      "home": 1100,
+      "desk": 600
+    },
+    "02 - الشلف": {
+      "home": 700,
+      "desk": 400
+    },
+    "03 - الأغواط": {
+      "home": 1000,
+      "desk": 600
+    },
+    "04 - أم البواقي": {
+      "home": 650,
+      "desk": 400
+    },
+    "05 - باتنة": {
+      "home": 700,
+      "desk": 400
+    },
+    "06 - بجاية": {
+      "home": 700,
+      "desk": 400
+    },
+    "07 - بسكرة": {
+      "home": 900,
+      "desk": 500
+    },
+    "08 - بشار": {
+      "home": 1100,
+      "desk": 600
+    },
+    "09 - البليدة": {
+      "home": 500,
+      "desk": 250
+    },
+    "10 - البويرة": {
+      "home": 700,
+      "desk": 400
+    },
+    "11 - تمنراست": {
+      "home": 1300,
+      "desk": 800
+    },
+    "12 - تبسة": {
+      "home": 700,
+      "desk": 400
+    },
+    "13 - تلمسان": {
+      "home": 800,
+      "desk": 400
+    },
+    "14 - تيارت": {
+      "home": 800,
+      "desk": 400
+    },
+    "15 - تيزي وزو": {
+      "home": 700,
+      "desk": 400
+    },
+    "16 - الجزائر": {
+      "home": 500,
+      "desk": 250
+    },
+    "17 - الجلفة": {
+      "home": 900,
+      "desk": 500
+    },
+    "18 - جيجل": {
+      "home": 700,
+      "desk": 400
+    },
+    "19 - سطيف": {
+      "home": 700,
+      "desk": 400
+    },
+    "20 - سعيدة": {
+      "home": 800,
+      "desk": 400
+    },
+    "21 - سكيكدة": {
+      "home": 600,
+      "desk": 400
+    },
+    "22 - سيدي بلعباس": {
+      "home": 700,
+      "desk": 400
+    },
+    "23 - عنابة": {
+      "home": 700,
+      "desk": 400
+    },
+    "24 - قالمة": {
+      "home": 600,
+      "desk": 400
+    },
+    "25 - قسنطينة": {
+      "home": 600,
+      "desk": 400
+    },
+    "26 - المدية": {
+      "home": 700,
+      "desk": 400
+    },
+    "27 - مستغانم": {
+      "home": 700,
+      "desk": 400
+    },
+    "28 - المسيلة": {
+      "home": 800,
+      "desk": 400
+    },
+    "29 - معسكر": {
+      "home": 700,
+      "desk": 400
+    },
+    "30 - ورقلة": {
+      "home": 900,
+      "desk": 500
+    },
+    "31 - وهران": {
+      "home": 800,
+      "desk": 400
+    },
+    "32 - البيض": {
+      "home": 800,
+      "desk": 500
+    },
+    "33 - إليزي": {
+      "home": 1300,
+      "desk": 600
+    },
+    "34 - برج بوعريريج": {
+      "home": 700,
+      "desk": 400
+    },
+    "35 - بومرداس": {
+      "home": 700,
+      "desk": 400
+    },
+    "36 - الطارف": {
+      "home": 700,
+      "desk": 400
+    },
+    "37 - تندوف": {
+      "home": 1300,
+      "desk": 600
+    },
+    "38 - تيسمسيلت": {
+      "home": 800,
+      "desk": 400
+    },
+    "39 - الوادي": {
+      "home": 900,
+      "desk": 500
+    },
+    "40 - خنشلة": {
+      "home": 700,
+      "desk": 500
+    },
+    "41 - سوق أهراس": {
+      "home": 700,
+      "desk": 400
+    },
+    "42 - تيبازة": {
+      "home": 700,
+      "desk": 400
+    },
+    "43 - ميلة": {
+      "home": 700,
+      "desk": 450
+    },
+    "44 - عين الدفلى": {
+      "home": 700,
+      "desk": 400
+    },
+    "45 - النعامة": {
+      "home": 800,
+      "desk": 500
+    },
+    "46 - عين تموشنت": {
+      "home": 800,
+      "desk": 400
+    },
+    "47 - غرداية": {
+      "home": 900,
+      "desk": 500
+    },
+    "48 - غليزان": {
+      "home": 700,
+      "desk": 400
+    },
+    "49 - تيميمون": {
+      "home": 1100,
+      "desk": 600
+    },
+    "50 - برج باجي مختار": {
+      "home": 1200,
+      "desk": 650
+    },
+    "51 - أولاد جلال": {
+      "home": 900,
+      "desk": 500
+    },
+    "52 - بني عباس": {
+      "home": 1100,
+      "desk": 600
+    },
+    "53 - عين صالح": {
+      "home": 1300,
+      "desk": 700
+    },
+    "54 - عين قزام": {
+      "home": 1300,
+      "desk": 700
+    },
+    "55 - توقرت": {
+      "home": 950,
+      "desk": 550
+    },
+    "56 - جانت": {
+      "home": 1100,
+      "desk": 500
+    },
+    "57 - المغير": {
+      "home": 950,
+      "desk": 550
+    },
+    "58 - المنيعة": {
+      "home": 900,
+      "desk": 400
+    }
+  },
+  "DISCOUNTS": {
+    "minQuantityForDiscount": 2,
+    "discountPerItem": 300,
+    "discountScope": "selected",
+    "discountProducts": [
+      1
+    ],
+    "promoDiscountProducts": [
+      3
+    ],
+    "enableQuantityDiscount": true,
+    "enablePromotionalDiscount": true,
+    "promotionalDiscountPercent": 20,
+    "promotionalDiscountedPrice": null,
+    "promoDiscountScope": "selected"
+  },
+  "STORE_INFO": {
+    "name": "BENAKRAB SHOPP",
+    "storeIcon": "👑",
+    "storeTitle": "👑 أحدث موديلات الأطفال",
+    "tagline": "متجر أفخم الملابس",
+    "phoneNumbers": [
+      "0671466489",
+      "0551102155"
+    ],
+    "facebookUrl": "https://www.facebook.com/share/r/1AUYY5favq/",
+    "instagramUrl": "https://www.instagram.com/p/DJCtlffoxj9/?igsh=MWoyaGFkYXRicmJrMQ==",
+    "messengerUrl": "",
+    "viberNumber": "0671466489"
+  },
+  "AVAILABLE_SIZES": [
+    "S",
+    "S1",
+    "S2",
+    "S3",
+    "M",
+    "M1",
+    "M2",
+    "M3",
+    "L",
+    "XL",
+    "XXL"
+  ],
+  "AVAILABLE_COLORS": [
+    "أبيض",
+    "أسود",
+    "أحمر",
+    "أزرق",
+    "أخضر",
+    "وردي",
+    "كما في الصورة"
+  ]
+};
