b29837afc9ef766ba7d9108b2c460e1f5473a22f
Dz-Store <benaakrabahcene@gmail.com>
2025-11-07 19:26:07 +0100

    تحديث إعدادات المتجر - 07/11/2025 19:26:06

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ const STORE_CONFIG = {
       "price": 6000,
       "image": "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-2-1762299261628-modle6.jpg",
       "description": "فستان الأفراح والمناسبات",
-      "availableSizes": [],
+      "availableSizes": [
+        "XXL",
+        "XL",
+        "L"
+      ],
       "availableColors": [
         "وردي",
         "أحمر",
