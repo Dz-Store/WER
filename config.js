0a13090628e76f9c30e84a8427e73396d7908c01
Dz-Store <benaakrabahcene@gmail.com>
2025-11-12 20:04:57 +0100

    تحديث إعدادات المتجر - 12/11/2025 20:04:54

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ const STORE_CONFIG = {
       "price": 5000,
       "image": "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-3-1762949625289-1000043662.jpg",
       "description": "",
-      "availableSizes": [],
+      "availableSizes": [
+        "S",
+        "S1",
+        "S2"
+      ],
       "availableColors": [
         "أزرق",
         "أحمر"
