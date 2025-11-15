115e06811a8373440ef2b232028c04f0700c8bad
Dz-Store <benaakrabahcene@gmail.com>
2025-11-15 17:09:04 +0100

    تحديث إعدادات المتجر - 15/11/2025 17:09:03

--- config.js
+++ config.js
@@ -13,7 +13,62 @@ const STORE_CONFIG = {
       ],
       "availableColors": [],
       "ageRanges": [],
-      "colorImages": {}
+      "colorImages": {
+        "أسود(Noir)": {
+          "images": [
+            "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-4-أسود(Noir)-1763222800574-modal1.jpeg"
+          ],
+          "ageRanges": [
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "S"
+            },
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "M"
+            },
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "L"
+            },
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "XL"
+            }
+          ]
+        },
+        "رمادي(Gris)": {
+          "images": [
+            "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-4-رمادي(Gris)-1763222871063-modal2.jpeg"
+          ],
+          "ageRanges": [
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "S"
+            },
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "M"
+            },
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "L"
+            },
+            {
+              "ageFrom": "A",
+              "ageTo": "A",
+              "size": "XL"
+            }
+          ]
+        }
+      }
     }
   },
   "DELIVERY_PRICES": {
