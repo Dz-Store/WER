5dfb453b2f2630b848b6918f6e128dc922491908
Dz-Store <benaakrabahcene@gmail.com>
2025-11-15 08:17:04 +0100

    تحديث إعدادات المتجر - 15/11/2025 08:17:04

--- config.js
+++ config.js
@@ -292,7 +292,9 @@ const STORE_CONFIG = {
     "minQuantityForDiscount": 2,
     "discountPerItem": 300,
     "discountScope": "selected",
-    "discountProducts": [],
+    "discountProducts": [
+      3
+    ],
     "promoDiscountProducts": [
       3
     ],
